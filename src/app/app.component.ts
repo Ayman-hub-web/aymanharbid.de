eb56810dedab6722f7232372f12917c1ce0cacae
Ayman <aymanab1971@gmail.com>
2022-01-10 10:45:04 +0100

    Added iamges und make Animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'aymanharbid.de';
+export class AppComponent implements OnInit{
+  images= [
+    'comArbeit.jfif', 'comProg.jfif', 'computer.jfif', 'computerArb.jfif', 'computerArbeit.jfif','newCom.jfif'
+  ];
+  currentImage= 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+  updateImage(){
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
 }
